20926f49d9cfb581508b900355e247f0306beac1
Deepinder-Singh841-Git <139867212+Deepinder-Singh841-Git@users.noreply.github.com>
2025-04-18 15:31:04 -0400

    Add files via upload

---
+++ models/routes/productRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = router;
